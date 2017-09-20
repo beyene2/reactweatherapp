940537fe02ad7dabf661fc42bd3ae8e94bba501a
Beyene Ayenew <beyenenw@gmail.com>
2017-09-20 18:56:58 +0000

    add Main and Nav files

--- app/app.jsx
+++ app/app.jsx
@@ -1,16 +1,14 @@
 var React=require('react');
 var ReactDom=require('react-dom');
+var Main=require("Main");
+var Nav=require("Nav");
 
-var objOne={
-    name:"Ben",
-    location:"Seattle"
-}
-var objTwo={
-    age:24,
-    ...objOne
-}
-console.log(objTwo);
+var {Route, Router,IndexRoute, hashHistory}=require("react-router");
+
 ReactDom.render(
-    <h1>This is the Boilerplate</h1>,
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>,
+        </Route>
+    </Router>,
     document.getElementById('app')
 );
